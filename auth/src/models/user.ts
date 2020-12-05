28ecddb203721add3b68cfe69fbe83da2b4815a9
David Armendariz <darmendariz1998@outlook.com>
2020-12-05 14:25:41 -0500

    Normalized user response

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -2,16 +2,28 @@ import mongoose from 'mongoose';
 import { UserAttrs, UserDoc, UserModel } from './types/user';
 import { Password } from '../services/password';
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
