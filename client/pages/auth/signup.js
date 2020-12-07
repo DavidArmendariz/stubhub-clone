a9d90c1a51045987bd1389c68f1b12af424899f5
David Armendariz <darmendariz1998@outlook.com>
2020-12-07 14:48:09 -0500

    Added error handling in signup component

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,14 +4,18 @@ import axios from 'axios';
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    const response = await axios.post('/api/users/signup', {
-      email,
-      password,
-    });
-    console.log(response.data);
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -34,6 +38,16 @@ const SignUp = () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
