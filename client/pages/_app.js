d7481281c5ed17e79c1c5c643f89581f5d574aca
David Armendariz <darmendariz1998@outlook.com>
2020-12-07 14:22:29 -0500

    Added bootstrap to client

---
+++ client/pages/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const AppComponent = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
 const App = () => {
-    return <h1>Landing Page</h1>
-}
+  return <h1>Landing Page</h1>;
+};
 
 export default App;
