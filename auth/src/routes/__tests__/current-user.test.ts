569505f694189acecfc31b54047c83553865c286
David Armendariz <darmendariz1998@outlook.com>
2020-12-07 13:39:07 -0500

    Completed tests for current-user route

--- auth/src/routes/__tests__/current-user.test.ts
+++ auth/src/routes/__tests__/current-user.test.ts
@@ -3,19 +3,22 @@ import { app } from '../../app';
 
 describe('When the user is signed', () => {
   test('should respond with details about the current user', async () => {
-    const authResponse = await request(app)
-      .post('/api/users/signup')
-      .send({
-        email: 'test@test.com',
-        password: 'password',
-      })
-      .expect(201);
-    const cookie = authResponse.get('Set-Cookie');
+    const cookie = await global.signup();
     const response = await request(app)
       .get('/api/users/currentuser')
       .set('Cookie', cookie)
       .send()
       .expect(200);
     expect(response.body.currentUser.email).toEqual('test@test.com');
+  });
+});
+
+describe('When the user is not authenticated', () => {
+  test('should respond with null', async () => {
+    const response = await request(app)
+      .get('/api/users/currentuser')
+      .send()
+      .expect(200);
+    expect(response.body.currentUser).toBeNull();
   });
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -25,3 +34,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+};
