1e650b596322f9367553aa0ad18191c4723e346b
David Armendariz <darmendariz1998@outlook.com>
2020-12-13 11:14:09 -0500

    Put header inside _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
+import { AUTH_ENDPOINTS } from '../constants/endpoints';
 
-const AppComponent = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+  const { data } = await buildClient(appContext.ctx).get(
+    AUTH_ENDPOINTS.currentUser
+  );
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    pageProps,
+    ...data,
+  };
 };
 
 export default AppComponent;
