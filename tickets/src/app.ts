61800a838c35734549fff14f93ab373b1b9ce8e3
David Armendariz <darmendariz1998@outlook.com>
2020-12-20 18:11:34 -0500

    Update tickets and common hash commit

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@daaptickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +16,7 @@ app.use(
   })
 );
 app.use(currentUser);
+app.use(showTicketRouter);
 
 app.use(createTicketRouter);
 
