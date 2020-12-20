a5d4c2390f02387d7bb4dc7f476f30d7902bec2d
David Armendariz <darmendariz1998@outlook.com>
2020-12-20 18:33:28 -0500

    Fixed test for showing tickets

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,10 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 404 if the ticket is not found', async () => {
-  await request(app).get('/api/tickets/not-existent-id').send().expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it('returns the ticket if the ticket is found', async () => {
