75e40527c2c37f5229cbb9a26066cc23944e1114
David Armendariz <darmendariz1998@outlook.com>
2020-12-20 18:48:17 -0500

    Added route to get all tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@daaptickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,10 +16,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-app.use(currentUser);
-app.use(showTicketRouter);
 
+app.use(currentUser);
+
+app.use(showTicketRouter);
 app.use(createTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
