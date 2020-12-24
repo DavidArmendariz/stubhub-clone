f6321e9c1089661a5fe4fcb5c5777cf02641101e
David Armendariz <darmendariz1998@outlook.com>
2020-12-23 20:41:47 -0500

    Added some nats-test code

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,30 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const clientId = randomBytes(4).toString('hex');
+
+const stan = nats.connect('ticketing', clientId, {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group', options);
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+    msg.ack();
+  });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
